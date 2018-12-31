8a1244382fe313fdaaf5d0187bca7620948fbe0f
Jair Benitez <jair.benitez@gmail.com>
2018-12-31 12:26:26 -0600

    Initial Commit

---
+++ 07-MathTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  07-MathTraining
+//
+//  Created by Jair Benítez on 31/12/18.
+//  Copyright © 2018 Jair Benítez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
